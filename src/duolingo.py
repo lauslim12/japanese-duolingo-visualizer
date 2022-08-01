fa41e160ae49fb716521309423e35e17d8026c72
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-08-01 17:09:53 +0700

    feat: Create classes for Duolingo and Store

---
+++ src/duolingo.py
@@ -0,0 +1,192 @@
+"""
+Custom API Client for Duolingo. This is necessary to access your own statistics in your Duolingo account.
+
+Essentially, there are three endpoints that will be used during the lifecycle of this API helper, which are:
+- `https://www.duolingo.com/login` -- to log in to the API.
+- `https://www.duolingo.com/users/<USERNAME>` -- to access the currently logged in user's data.
+- `https://www.duolingo.com/2017-06-30/users/<UID>` -- to access the currently logged in user's streak information and today's points / experience gained.
+
+Please use this code responsibly and do not spam Duolingo's servers.
+"""
+
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Any, NoReturn, Optional, Union
+
+import requests
+
+
+class CaptchaException(Exception):
+    """
+    Special exception for captcha responses. If this happens, it means that you
+    are probably caught in their spam filter and have to change your user agent. You also
+    have to log in again.
+    """
+
+
+class LoginException(Exception):
+    """
+    Special exception if you failed to log in to the API. This means that your credentials are either wrong,
+    or an internal server error happened at Duolingo's API.
+    """
+
+
+class NotFoundException(Exception):
+    """
+    Exception that will be thrown if the API returns a `404 Not Found`.
+    """
+
+
+class UnauthorizedException(Exception):
+    """
+    Exception that will be thrown if the API returns a `401 Unauthorized`.
+    """
+
+
+@dataclass
+class Duolingo:
+    """
+    REST API Client for Duolingo API. Please use responsibly and do not spam their servers.
+    """
+
+    # Base URL of Duolingo's API.
+    BASE_URL = "https://www.duolingo.com"
+
+    # Class members to be initialized in the `__init__` method. Remember, this is a `@dataclass`. For usage, it is recommended
+    # that you treat this like inserting `**kwargs`-style arguments.
+    username: str
+    password: Optional[str] = None
+    jwt: Optional[str] = None
+    session = requests.Session()
+    user_agent: str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    daily_progress: Optional[dict[str, Any]] = None
+    user_data: Optional[dict[str, Any]] = None
+
+    def __post_init__(self) -> None:
+        """
+        Post initialization, this class will try to log in into your Duolingo account with credentials your credentials
+        to fetch your data before placing them in `user_data` and `daily_progress` for future processing.
+        """
+        # A "constructor" -- trying to log in the first time the class is initialized, to
+        # abstract the whole 'login' process. We will try to check if the user has a JWT or
+        # not, if not, then we will log them in the usual way.
+        if not self.jwt:
+            self.jwt = self.login()
+
+        # Populate our `user_data` and `daily_progress` class attribute via API requests.
+        self.user_data = self.request(f"{self.BASE_URL}/users/{self.username}").json()
+        self.daily_progress = self.request(
+            f"{self.BASE_URL}/2017-06-30/users/{self.user_data['id']}"
+        ).json()
+
+    def request(
+        self, url: str, data: Optional[dict[str, Any]] = None
+    ) -> requests.Response:
+        """
+        Used to perform a request / API call to Duolingo's API. Handles all possible errors I could
+        think of, with the proper authorization (network headers) and request body.
+        """
+        # Creates required network headers to perform authenticated requests.
+        headers = {
+            "Authorization": f"Bearer {self.jwt}" if self.jwt is not None else "",
+            "User-Agent": self.user_agent,
+        }
+
+        # Prepare request.
+        request = requests.Request(
+            method="POST" if data else "GET",
+            url=url,
+            json=data,
+            headers=headers,
+            cookies=self.session.cookies,
+        )
+
+        # Send request.
+        response = self.session.send(request.prepare())
+
+        # Handle several errors: `401` and `404`.
+        if response.status_code == 401:
+            raise UnauthorizedException(
+                f"You are not authorized to access the resource with URL: '{url}'. Please try again with the correct credentials."
+            )
+        elif response.status_code == 404:
+            raise NotFoundException(
+                "The resource that you are looking for is not found."
+            )
+
+        # Handle an edge case: captcha lock-out!
+        if (
+            response.status_code == 403
+            and response.json().get("blockScript") is not None
+        ):
+            raise CaptchaException(
+                f"Request to '{url}' with user agent '{self.user_agent}' was blocked, and the API requests you to solve a captcha. Please try logging in again with a different user agent."
+            )
+
+        # Return proper response object.
+        return response
+
+    def login(self) -> Union[str, NoReturn]:
+        """
+        Logs in to the Duolingo API.
+
+        Please store the JWT (returned from this function) after this function returns. This is
+        intentionally done to prevent side-effects, keeping this function as pure as possible.
+        """
+        response = self.request(
+            f"{self.BASE_URL}/login",
+            data={"login": self.username, "password": self.password},
+        )
+        if "failure" in response.json():
+            raise LoginException(
+                "Failed to log in with your current credentials. Please check it and try again later."
+            )
+
+        return response.headers["jwt"]
+
+    def get_words(self) -> list[str]:
+        """
+        Gets all words one has learned. This process is done by querying the `user_data` class attribute.
+        """
+        words = []
+        for topic in self.user_data["language_data"]["ja"]["skills"]:
+            if topic["learned"]:
+                words += topic["words"]
+
+        return list(set(words))
+
+    def get_daily_experience_progress(self) -> dict[str, str]:
+        """
+        Gets daily experience progress. This process is done by querying the `daily_progress` class attribute.
+        """
+        # `xpGains` lists the lessons completed on the last day where lessons were done.
+        # We use the `streakData.updatedTimestamp`` to get the last "midnight", and get lessons after that.
+        reported_timestamp = self.daily_progress["streakData"]["updatedTimestamp"]
+        reported_midnight = datetime.fromtimestamp(reported_timestamp)
+        midnight = datetime.fromordinal(datetime.today().date().toordinal())
+
+        # Sometimes the update is marked into the future. When this is the case
+        # we fall back on the system time for midnight.
+        time_discrepancy = min(midnight - reported_midnight, timedelta(0))
+        update_cutoff = round((reported_midnight + time_discrepancy).timestamp())
+        lessons = [
+            lesson
+            for lesson in self.daily_progress["xpGains"]
+            if lesson["time"] > update_cutoff
+        ]
+
+        return {
+            "xp_goal": self.daily_progress["xpGoal"],
+            "xp_today": sum(x["xp"] for x in lessons),
+        }
+
+    def get_streak_info(self) -> dict[str, str]:
+        """
+        Gets current information about our daily streak from Duolingo. This process is done by querying the `user_data`
+        class attribute.
+        """
+        return {
+            "daily_goal": self.user_data["daily_goal"],
+            "site_streak": self.user_data["site_streak"],
+            "streak_extended_today": self.user_data["streak_extended_today"],
+        }
