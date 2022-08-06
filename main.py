a0f9a35daa7b892cd021bec9b56cf585e33d6771
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-08-06 22:19:15 +0700

    feat: Add console logging and refine error handling

--- main.py
+++ main.py
@@ -16,48 +16,70 @@ def main() -> None:
     - Store data in a JSON file with 2 indentation and sorted keys.
     - Outside of this script, please commit to GitHub (either via GitHub Actions or a standalone server cronjob).
     """
-    # If there's no username nor password or JWT.
-    if not environ.get("DUOLINGO_USERNAME") and (
-        not environ.get("DUOLINGO_PASSWORD") or not environ.get("DUOLINGO_JWT")
-    ):
-        print(
-            "Japanese Duolingo Visualizer script requires your username, and either your password or JWT."
+    try:
+        # If there's no username nor password or JWT.
+        if not environ.get("DUOLINGO_USERNAME") and (
+            not environ.get("DUOLINGO_PASSWORD") or not environ.get("DUOLINGO_JWT")
+        ):
+            raise Duolingo.LoginException(
+                "Japanese Duolingo Visualizer script requires your username, and either your password or JWT."
+            )
+
+        # Duolingo API client initialization.
+        lingo = Duolingo(
+            username=environ.get("DUOLINGO_USERNAME"),
+            password=environ.get("DUOLINGO_PASSWORD"),
+            jwt=environ.get("DUOLINGO_JWT"),
+            daily_experience_progress={},
+            user_data={},
         )
-        return
 
-    # Duolingo API client initialization.
-    lingo = Duolingo(
-        username=environ.get("DUOLINGO_USERNAME"),
-        password=environ.get("DUOLINGO_PASSWORD"),
-        jwt=environ.get("DUOLINGO_JWT"),
-        daily_experience_progress={},
-        user_data={},
-    )
+        # Log in to the API client.
+        lingo.login()
 
-    # Log in to the API client.
-    lingo.login()
+        # Print a message.
+        print(
+            "[JDV] Japanese Duolingo Visualizer script has successfully logged in to your account."
+        )
 
-    # Shape our data.
-    progress = {
-        "date": datetime.now().strftime("%Y/%m/%d"),
-        "time": datetime.now().strftime("%H:%M:%S"),
-        "experience": lingo.get_daily_experience_progress(),
-        "streak_information": lingo.get_streak_info(),
-        # This is large and can cause the JSON to bloat, as everyday I always learn new words.
-        # "learned_words": lingo.get_words(),
-        "number_of_learned_words": len(lingo.get_words()),
-    }
+        # Shape our data.
+        progress = {
+            "date": datetime.now().strftime("%Y/%m/%d"),
+            "time": datetime.now().strftime("%H:%M:%S"),
+            "experience": lingo.get_daily_experience_progress(),
+            "session_information": lingo.get_session_time(),
+            "streak_information": lingo.get_streak_info(),
+            # This is large and can cause the JSON to bloat, as everyday I always learn new words.
+            # "learned_words": lingo.get_words(),
+            "number_of_learned_words": len(lingo.get_words()),
+        }
 
-    # Save them to JSON.
-    store = Store(progress, path.join("data", "duolingo-progress.json"), [])
-    store.get_from_json_file()
-    store.process_json_data()
-    store.store_to_json_file()
+        # Print a message.
+        print(
+            "[JDV] Japanese Duolingo Visualizer script has successfully fetched the necessary information!"
+        )
 
-    # Print success message.
-    print(
-        "Japanese Duolingo Visualizer Script has finished running. Please check your specified `filename` path to see your newly updated data."
-    )
+        # Save them to JSON.
+        store = Store(progress, path.join("data", "duolingo-progress.json"), [])
+        store.get_from_json_file()
+        store.process_json_data()
+        store.store_to_json_file()
+
+        # Print success message.
+        print(
+            "[JDV] Japanese Duolingo Visualizer script is successful! Please check your specified `filename` path to see your newly updated data."
+        )
+    except (
+        Duolingo.CaptchaException,
+        Duolingo.LoginException,
+        Duolingo.NotFoundException,
+        Duolingo.UnauthorizedException,
+    ) as error:
+        print(f"[JDV] {error.__class__.__name__}: {error}")
+    except Exception as error:
+        print(f"[JDV] Unexpected Exception: {error.__class__.__name__}: {error}")
+    finally:
+        print("[JDV] Japanese Duolingo Visualizer script has finished running.")
 
 
 if __name__ == "__main__":
