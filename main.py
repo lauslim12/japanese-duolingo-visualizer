30ab961892f7fa27124e0749df120d48cc443197
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-08-04 23:15:49 +0900

    feat: Simplify and adjust runner, add exit codes

--- main.py
+++ main.py
@@ -1,61 +1,19 @@
 """Main runner, expected to be run as a command line program."""
 
-from copy import deepcopy
-from datetime import datetime
+from sys import exit
 from os import environ, path
 from traceback import format_exc
-from typing import Any, Tuple
 
-from src.duolingo import Duolingo
+
+from src.duolingo import (
+    DatabaseEntry,
+    Duolingo,
+    summary_to_progression,
+    sync_database_with_summaries,
+    user_data_to_streak_information,
+    progression_to_database_entry,
+)
 from src.store import Store
-
-
-def synchronize_xp_and_time(
-    json_data: list[dict[str, Any]], daily_progression: list[dict[str, Any]]
-) -> Tuple[list[dict[str, Any]], bool]:
-    """
-    Synchronizes own data with Duolingo's API for data we cannot have any disparities/differences with. The
-    time complexity of this function is O(N^2).
-
-    Keep in mind that `progression["time"]` is in UNIX timestamp, so it has to be converted into your usual
-    `YYYY-MM-DD` format so it uniforms with our own data.
-    """
-    new_json_data = deepcopy(json_data)
-    changed = False
-
-    for i, data in enumerate(json_data):
-        for progression in daily_progression["summaries"]:
-            progression_time = datetime.fromtimestamp(progression["date"]).strftime(
-                "%Y/%m/%d"
-            )
-
-            if data["date"] == progression_time:
-                # If found differences in `xp_today`.
-                if data["experience"]["xp_today"] != progression["gainedXp"]:
-                    new_json_data[i]["experience"]["xp_today"] = progression["gainedXp"]
-                    changed = True
-
-                # If found differences in `session_time`.
-                if (
-                    data["session_information"]["session_time"]
-                    != progression["totalSessionTime"]
-                ):
-                    new_json_data[i]["session_information"][
-                        "session_time"
-                    ] = progression["totalSessionTime"]
-                    changed = True
-
-                # If found differences in `number_of_sessions`.
-                if (
-                    data["session_information"]["number_of_sessions"]
-                    != progression["numSessions"]
-                ):
-                    new_json_data[i]["session_information"][
-                        "number_of_sessions"
-                    ] = progression["numSessions"]
-                    changed = True
-
-    return new_json_data, changed
 
 
 def main() -> None:
@@ -95,24 +53,18 @@ def main() -> None:
 
         # Print a message with the login type.
         print(
-            f"[JDV] Japanese Duolingo Visualizer script has logged in to your account with {lingo.login_method} method."
+            f"[JDV] Japanese Duolingo Visualizer script logged in to your account with {lingo.login_method} method."
         )
 
         # Fetch the data.
         lingo.fetch_data()
 
-        # Shape our data.
-        progress = {
-            "date": datetime.now().strftime("%Y/%m/%d"),
-            "experience": lingo.get_daily_experience_progress(),
-            # This is large and can cause the JSON to bloat, as everyday I always learn new words.
-            # For now, this is commented.
-            # "learned_words": lingo.get_words(),
-            "number_of_learned_words": len(lingo.get_words()),
-            "session_information": lingo.get_session_info(),
-            "streak_information": lingo.get_streak_info(),
-            "time": datetime.now().strftime("%H:%M:%S"),
-        }
+        # Shape our data. Indice `0` will always be there because it's Duolingo last active day data.
+        user_data = lingo.get_user_data()
+        summaries = lingo.get_summaries()
+        progression = summary_to_progression(summaries[0])
+        streak_information = user_data_to_streak_information(user_data)
+        entry = progression_to_database_entry(progression, streak_information)
 
         # Print a message.
         print(
@@ -120,37 +72,42 @@ def main() -> None:
         )
 
         # Prepare a file to take and store our data from/to.
-        store = Store(progress, path.join("data", "duolingo-progress.json"), [])
+        store = Store(path.join("data", "duolingo-progress-new.json"), [])
         store.get_from_json_file()
-        store.process_json_data()
 
-        # Synchronize our data, especially XP and session time.
-        new_data, changed = synchronize_xp_and_time(
-            store.json_content, lingo.daily_experience_progress
-        )
+        # Append the latest entry to the current database, and then sync it with the existing summaries.
+        store.content.append(entry)
+
+        # Synchronize our data.
+        database = [DatabaseEntry(**data) for data in store.content]
+        synced_database, changed = sync_database_with_summaries(summaries, database)
         if changed:
             print(
                 "[JDV] Japanese Duolingo Visualizer script has synchronized your data."
             )
 
         # Stores our data to a file.
-        store.json_content = new_data
+        store.content = [data.model_dump() for data in synced_database]
         store.store_to_json_file()
 
         # Print success message.
         print(
             "[JDV] Japanese Duolingo Visualizer script is successful! Please check your specified `filename` path to see your newly updated data."
         )
+        exit(0)
     except (
+        Duolingo.BreakingAPIChange,
         Duolingo.CaptchaException,
         Duolingo.LoginException,
         Duolingo.NotFoundException,
         Duolingo.UnauthorizedException,
     ) as error:
         print(f"[JDV] {error.__class__.__name__}: {error}")
+        exit(1)
     except Exception as error:
         print(f"[JDV] Unexpected Exception: {error.__class__.__name__}: {error}")
         print(format_exc())
+        exit(1)
     finally:
         print("[JDV] Japanese Duolingo Visualizer script has finished running.")
 
