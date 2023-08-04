1666088d424f835269baa8d70749b9346ce8a421
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-08-04 23:14:52 +0900

    feat: Remove redundant method, properly type variables

--- src/store.py
+++ src/store.py
@@ -2,7 +2,8 @@
 
 from dataclasses import dataclass
 from json import JSONDecodeError, dump, load
-from typing import Any
+
+from .duolingo import DatabaseEntry
 
 
 @dataclass
@@ -14,37 +15,28 @@ class Store:
     class StoreException(Exception):
         """Exception that will be raised if unwanted things happen during the usage of this class."""
 
-    data: dict[str, Any]
     filename: str
-    json_content: list[dict[str, Any]]
+    content: list[DatabaseEntry]
 
     def get_from_json_file(self) -> None:
         """
         Attempts to read from `filename` to parse the JSON that is available in that file. If
         the program caught an environment error (parent of `IOError`, `OSError`, and `WindowsError` if
         applicable), it will raise an exception and will not continue. If failed to parse the JSON,
-        then `json_content` will be set to an empty array `[]`.
+        then `content` will be set to an empty array `[]`.
         """
         try:
             with open(self.filename, "r", encoding="UTF-8") as file:
                 try:
-                    self.json_content = load(file)
+                    self.content = load(file)
                 except JSONDecodeError as error:
-                    self.json_content = []
+                    self.content = []
         except EnvironmentError as error:
             raise self.StoreException(f"Environment error: {error}.")
 
     def store_to_json_file(self) -> None:
         """
-        Stores `json_content` to `filename`. Before you do this, please do `process_json_data` first.
-        If you're wondering why I code like this, it is because I want to conform to the Single Responsibility
-        Principle. Sadly, this is not a pure function as it involves input/output.
+        Stores `content` to `filename`.
         """
         with open(self.filename, "w", encoding="UTF-8") as file:
-            dump(self.json_content, file, ensure_ascii=False, indent=2, sort_keys=True)
-
-    def process_json_data(self) -> None:
-        """
-        Appends `json_content` with `data`.
-        """
-        self.json_content.append(self.data)
+            dump(self.content, file, ensure_ascii=False, indent=2, sort_keys=True)
