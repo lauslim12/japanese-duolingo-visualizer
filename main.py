516c4eb71f58def887eb18a0a5dcf219833125c5
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2024-06-19 23:10:57 +0900

    refactor: Rewrite main runner

--- main.py
+++ main.py
@@ -1,115 +1,129 @@
-"""Main runner, expected to be run as a command line program."""
-
-from sys import exit
 from os import environ, path
 from traceback import format_exc
 
+from pydantic import ValidationError
 
-from src.duolingo import (
-    DatabaseEntry,
-    Duolingo,
-    summary_to_progression,
-    sync_database_with_summaries,
-    user_data_to_streak_information,
-    progression_to_database_entry,
+from src.api import (
+    APIClient,
+    CaptchaException,
+    LoginException,
+    NotFoundException,
+    UnauthorizedException,
 )
-from src.store import Store
+from src.database import Database
+from src.schema import DatabaseEntry, Statistics, Summary, User
+from src.synchronizer import check_database_change, sync_database_with_summaries
+
+
+def log(message: str) -> None:
+    print(f"[JDV] {message}")
+
+
+def run() -> tuple[bool, bool]:
+    # Initialize environment.
+    base_api_url = "https://www.duolingo.com"
+    username = environ["DUOLINGO_USERNAME"]
+    credential, passwordless = (
+        (credential, True)
+        if (credential := environ.get("DUOLINGO_JWT")) is not None
+        else (environ["DUOLINGO_PASSWORD"], False)
+    )
+
+    # Declare paths.
+    progression_database_path = path.join("data", "duolingo-progress.json")
+    statistics_database_path = path.join("data", "statistics.json")
+
+    # Initialize required infrastructures.
+    api = APIClient(base_url=base_api_url)
+    progression_database = Database(filename=progression_database_path)
+    statistics_database = Database(filename=statistics_database_path)
+
+    # If the supplied credential is the password, login to Duolingo first.
+    token, passwordless = (
+        (credential, True) if passwordless else (api.login(username, credential), False)
+    )
+
+    # Get the possible data.
+    raw_user, raw_summary = api.fetch_data(username, token)
+
+    # Transform them into our internal schema.
+    user = User.to_user(raw_user)
+    summaries = [Summary(**summary) for summary in raw_summary["summaries"]]
+
+    # Get all existing data from the database. Add the new data to the end of the database
+    # declaratively. `0` means the first entry, or today (when the script is run).
+    database_entries = [
+        *[DatabaseEntry(**entry) for entry in progression_database.get()],
+        DatabaseEntry.create_now(summaries[0], user.site_streak),
+    ]
+
+    # Synchronize the database with the summaries.
+    synchronized_database = sync_database_with_summaries(database_entries, summaries)
+
+    # Check whether we have synchronized the data or not.
+    is_database_changed = check_database_change(synchronized_database, database_entries)
+
+    # Store the synchronized database in our repository.
+    progression_database.set(
+        [DatabaseEntry.to_dict(entry) for entry in synchronized_database]
+    )
+
+    # On the other hand, get all of the statistics of the cron run, and then immutably
+    # add the current cron statistics.
+    statistics_entries = Statistics(
+        datetime={
+            **statistics_database.get()["datetime"],
+            **Statistics.create_datetime_now(),
+        }
+    )
+
+    # Store the statistics in our repository.
+    statistics_database.set(Statistics.to_dict(statistics_entries))
+
+    # Return flags from the program to consolidate the print statements in the outer loop,
+    # minimizing side effects.
+    return passwordless, is_database_changed
 
 
 def main() -> None:
-    """
-    What we want:
-    - Validate our credentials.
-    - Login to Duolingo with our credentials in our environment.
-    - Fetch all necessary data.
-    - Store data in a JSON file with 2 indentation and sorted keys.
-    - Outside of this script, please commit to GitHub (either via GitHub Actions or a standalone server cronjob).
-    """
+    log("Script is starting and running now.")
     try:
-        # If there's no username nor password or JWT.
-        if not environ.get("DUOLINGO_USERNAME") and (
-            not environ.get("DUOLINGO_PASSWORD") or not environ.get("DUOLINGO_JWT")
-        ):
-            raise Duolingo.LoginException(
-                "Japanese Duolingo Visualizer script requires your username, and either your password or JWT."
-            )
+        passwordless, is_database_changed = run()
+        match passwordless:
+            case True:
+                log("Script authenticated with your JWT.")
+            case False:
+                log("Script authenticated with your password. Please change it to JWT.")
 
-        # Duolingo API client initialization.
-        lingo = Duolingo(
-            username=environ.get("DUOLINGO_USERNAME"),
-            password=environ.get("DUOLINGO_PASSWORD"),
-            jwt=environ.get("DUOLINGO_JWT"),
-            daily_experience_progress={},
-            user_data={},
+        match is_database_changed:
+            case True:
+                log(
+                    "Script found discrepancies between current data and online data. Synchronization is done automatically."
+                )
+            case False:
+                log(
+                    "Script did not find discrepancies between current data and online data. Synchronization not required."
+                )
+
+        log(
+            "Script run successfully! Please check the specified path to see your newly updated data."
         )
-
-        # Log in to the API client.
-        lingo.login()
-
-        # Print a message.
-        print(
-            "[JDV] Japanese Duolingo Visualizer script has successfully logged in to your account."
+    except ValidationError as error:
+        log(
+            f"Error encountered when parsing data. Potentially, a breaking API change: {error}"
         )
-
-        # Print a message with the login type.
-        print(
-            f"[JDV] Japanese Duolingo Visualizer script logged in to your account with {lingo.login_method} method."
-        )
-
-        # Fetch the data.
-        lingo.fetch_data()
-
-        # Shape our data. Indice `0` will always be there because it's Duolingo last active day data.
-        user_data = lingo.get_user_data()
-        summaries = lingo.get_summaries()
-        progression = summary_to_progression(summaries[0])
-        streak_information = user_data_to_streak_information(user_data)
-        entry = progression_to_database_entry(progression, streak_information)
-
-        # Print a message.
-        print(
-            "[JDV] Japanese Duolingo Visualizer script has successfully fetched the necessary information!"
-        )
-
-        # Prepare a file to take and store our data from/to.
-        store = Store(path.join("data", "duolingo-progress.json"), [])
-        store.get_from_json_file()
-
-        # Append the latest entry to the current database, and then sync it with the existing summaries.
-        store.content.append(entry.model_dump())
-
-        # Synchronize our data.
-        database = [DatabaseEntry(**data) for data in store.content]
-        synced_database, changed = sync_database_with_summaries(summaries, database)
-        if changed:
-            print(
-                "[JDV] Japanese Duolingo Visualizer script has synchronized your data."
-            )
-
-        # Stores our data to a file.
-        store.content = [data.model_dump() for data in synced_database]
-        store.store_to_json_file()
-
-        # Print success message.
-        print(
-            "[JDV] Japanese Duolingo Visualizer script is successful! Please check your specified `filename` path to see your newly updated data."
-        )
-        exit(0)
     except (
-        Duolingo.BreakingAPIChange,
-        Duolingo.CaptchaException,
-        Duolingo.LoginException,
-        Duolingo.NotFoundException,
-        Duolingo.UnauthorizedException,
+        CaptchaException,
+        LoginException,
+        NotFoundException,
+        UnauthorizedException,
     ) as error:
-        print(f"[JDV] {error.__class__.__name__}: {error}")
-        exit(1)
+        log(f"{error.__class__.__name__}: {error}")
     except Exception as error:
-        print(f"[JDV] Unexpected Exception: {error.__class__.__name__}: {error}")
-        print(format_exc())
-        exit(1)
+        log(f"Unexpected Exception: {error.__class__.__name__}: {error}")
+        log(format_exc())
     finally:
-        print("[JDV] Japanese Duolingo Visualizer script has finished running.")
+        log("Japanese Duolingo Visualizer script has finished running.")
 
 
 if __name__ == "__main__":
